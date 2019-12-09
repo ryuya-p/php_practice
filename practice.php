5bf12170b02fad8b001341a2981a460f5f5d9edd
Ryuya <r.yasooka1@gmail.com>
2019-12-09 06:47:20 +0000

    PHP02課題完了

---
+++ practice.php
@@ -0,0 +1,32 @@
+<?php
+$a = 3;
+$b = 7;
+echo $a + $b;
+
+$array_month = ["January", "February", "February", "April", "May", "June", "July", "August", "September", "October", "November", "December",];
+echo $array_month[7];
+
+$hello = "Hello,";
+$name = "ryuya";
+$world = "'s World!";
+echo $hello.$name.$world;
+
+$tech_boost = "tech";
+$tech_boost .= " boost";
+echo $tech_boost;
+
+$calendar_2018 = [ 
+  "January" => "1月",
+  "February" => "2月",
+  "March" => "3月",
+  "April" => "4月",
+  "May" => "5月",
+  "June" => "6月",
+  "July" => "7月",
+  "August" => "8月",
+  "September" => "9月",
+  "October" => "10月",
+  "November" => "11月",
+  "December" => "12月",
+];
+echo $calendar_2018["December"];
