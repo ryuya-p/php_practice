8b6d4b24775762f6e6b71feaa25d4fc20841912d
Ryuya <r.yasooka1@gmail.com>
2019-12-11 06:44:00 +0000

    PHP03課題完了

---
+++ practice03.php
@@ -0,0 +1,33 @@
+<?php
+$name = "ryuya";
+switch ($name) {
+  case "ryuya":
+   echo "私は あなたの名前 です";
+   break;
+   default:
+     echo "あなたの名前ではありません";
+}
+echo "\n";
+
+$total = 0;
+for ($i = 1; $i <= 10000; $i++) {
+    $total += $i;
+}
+echo $total;
+echo "\n";
+
+$fruits = array("apple", "strawberry", "banana", "grape", "peach",);
+foreach($fruits as $fruit ){
+    echo $fruit." ";
+}
+echo "\n";
+
+$start = 1;
+$end = 101;
+
+for($i = $start; $i < $end; $i++ ){
+  if($i % 5 == 0){
+  echo $i;
+  echo "\n";
+  }
+}
